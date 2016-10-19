0d637602c02d30dcbf6037d81eb7335789c314ed
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-19 14:01:12 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("Ilm on õues päris ilus");
+        int tund, minut;
+        tund = 13;
+        minut = 23;
+        System.out.println("Kell on " + tund + "." + minut);
+        System.out.println("Kui palju minuteid on möödas südaööst");
+               System.out.println("Südaööst on möödas " + tund*60 + minut + " minutit!");
+        System.out.println("Mitu % tunnist on möödas?");
+        System.out.println(minut*60 / 100);
+        int vanus;
+                vanus = 2016-1987;
+                System.out.println("Ma olen " + vanus + " aastat vana.");
+    }
+
+}
