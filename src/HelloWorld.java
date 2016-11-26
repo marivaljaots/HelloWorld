5d4428147a2598e51b9adb39bf752c0cf05756e2
Mari <marivaljaots@gmail.com>
2016-11-26 20:15:27 +0200

    HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,32 +4,6 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello World");
-        System.out.println("Ilm on õues päris ilus");
-        int tund, minut;
-        tund = 13;
-        minut = 23;
-        int paev =0;
-        System.out.println("Kell on " + tund + "." + minut);
-        System.out.println("Kui palju minuteid on möödas südaööst");
-               System.out.println("Südaööst on möödas " + tund*60 + minut + " minutit!");
-        System.out.println("Mitu % tunnist on möödas?");
-        System.out.println(minut*60 / 100);
-        double minut1;
-        minut1 = 23.0;
-        System.out.println(minut1*60/100);
-        int vanus;
-                vanus = 2016-1987;
-                System.out.println("Ma olen " + vanus + " aastat vana.");
-        System.out.println(0.1 * 10);
-        System.out.println(0.1 + 0.1 + 0.1 + 0.1 + 0.1
-                + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
-        System.out.println(1 + 2 + " Õpetamist vajat tudengit istuvad ühes reas");
-        System.out.println("Õpetamist vajavaid tudengeid istub ühes reas " +1+2+38);
-System.out.println(17*85-93/4);
-        System.out.print((17*8)-(93/4));
-
-
-
     }
 
 }
